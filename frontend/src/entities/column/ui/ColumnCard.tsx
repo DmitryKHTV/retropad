5bc60e858aa83582d8b7dba6259cd70def5e12bb
khtv <dimah877@gmail.com>
2026-07-01 23:51:43 +0300

    refactor(column): rename Column entity component to ColumnCard

---
+++ frontend/src/entities/column/ui/ColumnCard.tsx
@@ -0,0 +1,36 @@
+import {Fragment, ReactNode} from "react";
+import {ColumnWithStickers} from "@/entities/column/model";
+import {Sticker, StickerCard} from "@/entities/sticker";
+import cls from "./ColumnCard.module.css";
+
+type ColumnCardProps = ColumnWithStickers & {
+    titleSlot?: ReactNode;
+    actions?: ReactNode;
+    renderSticker?: (sticker: Sticker) => ReactNode;
+};
+
+export const ColumnCard = (props: ColumnCardProps) => {
+    const {title, stickers, titleSlot, actions, renderSticker} = props;
+
+    return (
+        <div className={cls.wrapper}>
+            <div className={cls.title}>
+                {titleSlot ?? <p>{title}</p>}
+            </div>
+            <div className={cls.actions}>
+                {actions}
+            </div>
+            <div className={cls.stickers}>
+                {stickers.length > 0
+                    ? stickers.map((sticker: Sticker) => (
+                        <Fragment key={`sticker-${sticker.id}`}>
+                            {renderSticker
+                                ? renderSticker(sticker)
+                                : <StickerCard content={sticker.content} />}
+                        </Fragment>
+                    ))
+                    : <p className={cls.noStickersText}>No stickers yet</p>}
+            </div>
+        </div>
+    );
+};
