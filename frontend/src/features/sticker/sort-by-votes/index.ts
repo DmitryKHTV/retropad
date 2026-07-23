ae9080a0d3b8221fac040af0300482549c0c08dd
khtv <dimah877@gmail.com>
2026-07-23 22:45:19 +0300

    feat: button for sorting stickers by votes

---
+++ frontend/src/features/sticker/sort-by-votes/index.ts
@@ -0,0 +1,3 @@
+export {SortByVotesToggle} from "./ui/SortByVotesToggle";
+export {useSortByVotes} from "./model/use-sort-by-votes";
+export {sortStickersByVotes} from "./lib/sort-stickers-by-votes";
